46b85945aa683e2c94f2a3301ae5680c50e91f74
ndobrosa <ndobrosa@gmail.com>
2018-09-09 14:38:05 -0600

    Jets v2: fixed a bug with time not printing minutes

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 ###Week 2 AHomework for Skill Distillery
 
 This program displays and manages a fleet of Jet objects.
+
+#### Overview
+The program allows the user to manage a fleet of jet objects. Firstly, the user is prompted to chose what task they would like the program to execute. If the user presses 1 ("List fleet"), a list of all Jet objects is displayed. The second option lists the information about each object, as well as the maximum flying time. Pressing 3 prints information about the Jet object with the highest speed. The fourth option prints info about the Jet object with the longest range. The fifth option prints a message informing the user that all Cargo planes are loading. The sixth option prints a message informing the user that all fighter jets are fighting. Pressing 7 allows the user to create a new Jet object. Finally, pressing 8 exits the program. 
+
+#### Technologies Used
+Java, Eclipse;
+
+#### Lessons Learned
+Polymorphism, Inheritance, Encapsulation, Abstract Classes;
--- src/com/skilldistillery/jets/Jet.java
+++ src/com/skilldistillery/jets/Jet.java
@@ -1,65 +1,73 @@
 package com.skilldistillery.jets;
 
 public abstract class Jet {
-private String model;
-private double speed;
-private int range;
-private long price;
+	private String model;
+	private double speed;
+	private int range;
+	private long price;
 
-public String getModel() {
-	return model;
-}
+	public String getModel() {
+		return model;
+	}
 
-public void setModel(String model) {
-	this.model = model;
-}
+	public void setModel(String model) {
+		this.model = model;
+	}
 
-public double getSpeed() {
-	return speed;
-}
+	public double getSpeed() {
+		return speed;
+	}
 
-public void setSpeed(double speed) {
-	this.speed = speed;
-}
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
 
-public int getRange() {
-	return range;
-}
+	public int getRange() {
+		return range;
+	}
 
-public void setRange(int range) {
-	this.range = range;
-}
+	public void setRange(int range) {
+		this.range = range;
+	}
 
-public long getPrice() {
-	return price;
-}
+	public long getPrice() {
+		return price;
+	}
 
-public void setPrice(long price) {
-	this.price = price;
-}
+	public void setPrice(long price) {
+		this.price = price;
+	}
 
-public Jet (String model, double speed, int range, long price) {
-	this.model = model;
-	this.speed = speed;
-	this.range = range;
-	this.price = price;
-}
+	public Jet(String model, double speed, int range, long price) {
+		this.model = model;
+		this.speed = speed;
+		this.range = range;
+		this.price = price;
+	}
 
+	public void fly() {
+		double timeFlying = range / speed;
+		int intTimeFlying = (int) timeFlying;
+		String minTimeFlying = "" + (int) ((timeFlying - intTimeFlying) * 60) + " minutes.";
+		String message = "model: " + model + "top speed: " + speed + "range: " + range + "tprice: " + price
+				+ "  time before crash: ";
 
+		if ((int) timeFlying < 1) {
+			System.out.println(message + minTimeFlying);
+		} else if ((int) timeFlying == 1) {
+			System.out.println(message + intTimeFlying + " hour " + minTimeFlying);
+		} else {
+			System.out.println(message + intTimeFlying + " hours " + minTimeFlying);
+		}
+	}
 
-public void fly () {
-	System.out.println("model: " + model + "  top speed: " + speed + "  range: " + range + "\tprice: " + price + "  time before crash: " + (range/speed));
-}
+	public double getSpeedInMach() {
 
-public double getSpeedInMach() {
-	
-	return 0;
-}
+		return 0;
+	}
 
-@Override
-public String toString() {
-	return "Jet model=" + model + "    speed=" + speed + "    range=" + range + "    price=" + price;
-}
-
-
+	@Override
+	public String toString() {
+		return "Jet model=" + model + "    speed=" + speed + "    range=" + range + "    price=" + price;
+	}
 }
